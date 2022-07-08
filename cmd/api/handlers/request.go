a2abcaf638241da1e08532dc754c394ef281175c
Carlos Eduardo Martinez Cantor <carlos.martinez@arandasoft.com>
2022-07-08 04:36:21 -0500

    correct documentation

--- cmd/api/handlers/request.go
+++ cmd/api/handlers/request.go
@@ -3,12 +3,12 @@ package handlers
 // swagger:model
 type StringFriendsRequest struct {
 	// StringX first value entered
-	// example: "tokyo"
+	// example: tokyo
 	// required: true
 	StringX string `json:"x" binding:"required"`
 
 	// StringY second value entered
-	// example: "kyoto"
+	// example: kyoto
 	// required: true
 	StringY string `json:"y" binding:"required"`
 }
@@ -16,7 +16,7 @@ type StringFriendsRequest struct {
 // swagger:model
 type OrderNameRequest struct {
 	// Names list of names separated by ,
-	// example: "Luis,Camilo,Andres,Laura"
+	// example: Luis,Camilo,Andres,Laura
 	// required: true
 	Names string `json:"names" binding:"required"`
 }
